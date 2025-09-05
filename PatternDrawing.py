9bb4bddad9c7367f74a62a344628542e06182d53
sujal399 <s392965@students.cdu.edu.au>
2025-09-05 13:08:16 +0930

    function for factorial edge

--- PatternDrawing.py
+++ PatternDrawing.py
@@ -15,3 +15,21 @@ class FractalPolygonDrawer:
         self.t = turtle.Turtle()
         self.t.speed(0)  
         self.t.hideturtle()  
+    
+    def draw_fractal_edge(self, length, depth):
+     
+        if depth == 0:
+            self.t.forward(length)
+            return
+ 
+        segment_length = length / 3.0
+ 
+       
+        self.draw_fractal_edge(segment_length, depth - 1)
+        self.t.right(60)
+        self.draw_fractal_edge(segment_length, depth - 1)
+        self.t.left(120)
+        self.draw_fractal_edge(segment_length, depth - 1)
+        self.t.right(60)
+        self.draw_fractal_edge(segment_length, depth - 1)
+        
